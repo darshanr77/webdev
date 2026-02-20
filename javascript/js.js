ae38720ee324e73cc2e9381ee822f38298df1957
Darshan <darshankok46@gmail.com>
2026-02-20 14:36:30 +0530

    trim trims whitespaces frmo both side of str

--- javascript/js.js
+++ javascript/js.js
@@ -1,1 +1,27 @@
 alert("hello");
+let promp =  prompt("this is a prompt!");
+console.log(promp);
+console.log("Normal message");
+console.info("Information");
+console.warn("Warning message");
+console.error("Error message");
+console.debug("Debug message");
+
+console.count("Click");
+console.count("Click");
+
+console.time("Timer");
+for(let i=0;i<1000000;i++){}
+console.timeEnd("Timer");
+
+console.assert(10 < 5, "Condition is false");
+
+console.group("User Details");
+console.log("Name: Darshan");
+console.log("Age: 20");
+console.groupEnd();
+
+console.table([
+  {name: "Darshan", age: 20},
+  {name: "Rahul", age: 22}
+]);
